f563058102711638a0426a67637f98f9738c69c0
Alex Karpinski <akarpins@u.rochester.edu>
2014-12-16 13:55:25 +0800

    rework long-box for contact/resume to remove icons

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions:
   {
-    'toggleContact': function(modalName){
-      return this.render(modalName, {
-        into: 'index',
-        outlet: 'contactPopover'
-      });
-    }
   }
 });

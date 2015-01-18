00944278cf75ca5098908cd9ed5fafe82c5b0b36
Alex Karpinski <akarpins@u.rochester.edu>
2015-01-18 23:34:38 +0800

    add data-driven skills model.

---
+++ app/controllers/skills.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  // nextObject: function(category) {
+  //   return this.get('model')['key']pop()
+  // }
+});
--- app/routes/skills.js
+++ app/routes/skills.js
@@ -1,4 +1,65 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+  model: function(){
+    // List of categories (pro, personal, toy) each containing a
+    // list of tool-name/file-name pairs for iteration in template
+    var skill_categories = [
+      {
+        name: "Professional Use", 
+        skills: [
+          {name: "Python", file:"images/tools/python.svg"},
+          {name: "Chef", file:"images/tools/chef.svg"},
+          {name: "Linux", file:"images/tools/penguin.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Javascript", file:"images/tools/javascript.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"}
+         ]
+      },
+      {
+        name: "Personal Use", 
+        skills: [
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"}
+         ]
+      },
+      {
+        name: "Toys", 
+        skills: [
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"},
+          {name: "Git", file:"images/tools/git.svg"}
+         ]
+      },
+   ];
+   skill_categories = _.map(skill_categories, function(category) {
+    var grouped_skills = [];
+    while (category.skills.length > 0) {
+      grouped_skills.push( category.skills.splice(0, 5));
+    } 
+    category.skill_groups = grouped_skills;
+    return category;
+   });
+   return skill_categories;
+ }
 });

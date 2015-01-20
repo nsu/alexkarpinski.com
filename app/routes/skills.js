0bb33e8701fdcb3371794803fe691322e07e0fbc
Alex Karpinski <akarpins@u.rochester.edu>
2015-01-20 13:27:02 +0800

    fill-in skills model, add a promo-box, and swap out ugly images for pretty ones

--- app/routes/skills.js
+++ app/routes/skills.js
@@ -14,41 +14,44 @@ export default Ember.Route.extend({
           {name: "Linux", file:"images/tools/penguin.svg"},
           {name: "Git", file:"images/tools/git.svg"},
           {name: "Javascript", file:"images/tools/javascript.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"}
+
+          {name: "Django", file:"images/tools/django.svg"},
+          {name: "jQuery", file:"images/tools/jquery.svg"},
+          {name: "HTML/CSS", file:"images/tools/html.svg"},
+          {name: "AWS", file:"images/tools/aws.svg"},
+          {name: "Testing", file:"images/tools/tdd.svg"},
+
+          {name: "NGinx", file:"images/tools/nginx.svg"},
+          {name: "Apache", file:"images/tools/apache.svg"},
+          {name: "DNS", file:"images/tools/dns.svg"},
+          {name: "Vagrant", file:"images/tools/vagrant.svg"},
+          {name: "Jira", file:"images/tools/jira.svg"}
          ]
       },
       {
         name: "Personal Use", 
         skills: [
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"}
+          {name: "Ruby", file:"images/tools/Ruby.svg"},
+          {name: "SASS", file:"images/tools/sass.svg"},
+          {name: "EmberJS", file:"images/tools/ember.svg"},
+          {name: "NodeJS", file:"images/tools/nodejs.svg"},
+          {name: "ExpressJS", file:"images/tools/express.svg"},
+
+          {name: "MongoDB", file:"images/tools/mongodb.svg"},
+          {name: "CouchDB", file:"images/tools/couchdb.svg"},
+          {name: "Bootstrap", file:"images/tools/bootstrap.svg"},
+          {name: "Foundation", file:"images/tools/foundation.svg"},
+          {name: "Mercurial", file:"images/tools/mercurial.svg"}
          ]
       },
       {
         name: "Toys", 
         skills: [
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"},
-          {name: "Git", file:"images/tools/git.svg"}
+          {name: "Salt", file:"images/tools/salt.svg"},
+          {name: "Angular", file:"images/tools/angular.svg"},
+          {name: "Broccoli", file:"images/tools/broccoli.svg"},
+          {name: "Rails", file:"images/tools/rails.svg"},
+          {name: "Illustrator", file:"images/tools/illustrator.svg"},
          ]
       },
    ];

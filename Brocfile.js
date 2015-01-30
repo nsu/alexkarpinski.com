d35273620280bbba4ab51e96db693db8200d7aa9
Alex Karpinski <akarpins@u.rochester.edu>
2015-01-30 13:37:50 +0800

    upgrade version of lodash for chunk functionality

--- Brocfile.js
+++ Brocfile.js
@@ -17,7 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 //
-app.import('bower_components/lodash/dist/lodash.min.js');
+app.import('bower_components/lodash/lodash.min.js');
 app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {
   destDir: 'fonts'
 });
